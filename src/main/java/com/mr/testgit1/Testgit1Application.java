eb0e91c3baf8fe615801093ac89b2f20f4d04cda
yangxy2000 <yangxy_19910928@163.com>
2020-04-18 14:08:06 +0800

    create local repository

---
+++ src/main/java/com/mr/testgit1/Testgit1Application.java
@@ -0,0 +1,13 @@
+package com.mr.testgit1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testgit1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testgit1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mr/testgit1/Testgit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.testgit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testgit1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
